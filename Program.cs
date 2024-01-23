0966e1262419a2f5e5e8ce8d7b78dafc9432daab
Gordon Beeming <gpg@gordonbeeming.com>
2024-01-23 19:25:11 +1000

    init code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+static class Program
+{
+  public static void Main()
+  {
+    var rnd = new Random();
+    var rnd2 = new Random();
+    Console.WriteLine(rnd.Next());
+    Console.WriteLine(rnd2.Next());
+  }
+}
